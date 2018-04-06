7f7b9cc28604ac222d51ce5476839ed580452e12
Bastian Koppelmann <kbastian@mail.uni-paderborn.de>
2018-04-06 14:32:35 +0200

    Add GPL
    Signed-off-by: Bastian Koppelmann <kbastian@mail.uni-paderborn.de>

--- target/tricore/trans_tricore_16.inc.c
+++ target/tricore/trans_tricore_16.inc.c
@@ -1,3 +1,20 @@
+/*
+ *  Copyright (c) 2018 Bastian Koppelmann Paderborn University
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
 bool trans16_src_add(DisasContext *ctx, arg_src_add *a, uint16_t insn)
 {
     gen_addi_d(cpu_gpr_d[a->s1_d], cpu_gpr_d[a->s1_d], a->const4);
