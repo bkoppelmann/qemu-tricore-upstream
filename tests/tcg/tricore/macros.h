a3f899847d3c12dbafc288d5b2a6897af39c2921
Bastian Koppelmann <kbastian@mail.uni-paderborn.de>
2018-04-30 21:42:24 +0200

    tests/tcg/tricore: remove magic values for regs from macros
    Signed-off-by: Bastian Koppelmann <kbastian@mail.uni-paderborn.de>

--- tests/tcg/tricore/macros.h
+++ tests/tcg/tricore/macros.h
@@ -1,43 +1,74 @@
+#define REG_RS1 %d0
+#define REG_RS2 %d1
+#define REG_CALC_RESULT %d1
+#define REG_CALC_PSW %d2
+#define REG_CORRECT_PSW %d3
+#define REG_TEMP_LI %d10
+#define REG_TEMP %d11
+#define REG_TEST_NUM %d14
+#define REG_CORRECT_RESULT %d15
+#define REG_DEV_ADDR %a15
 
 #define TEST_CASE(num, testreg, correct, code...) \
 test_ ## num: \
     code;     \
-    mov %d15 correct; \
-    mov %d14, num;    \
-    jne testreg, %d15, fail \
+    LI(REG_CORRECT_RESULT, correct) \
+    mov REG_TEST_NUM, num;    \
+    jne testreg, REG_CORRECT_RESULT, fail \
+
+#define TEST_PSW(num, correct) \
+    mfcr REG_CALC_PSW, $psw;       \
+    LI(REG_CORRECT_PSW, correct)      \
+    mov REG_TEST_NUM, num;        \
+    jne REG_CALC_PSW, REG_CORRECT_PSW, fail;   \
+
+#define TEST_CASE_PSW(num, testreg, correct, correct_psw, code...) \
+test_ ## num: \
+    code;     \
+    LI(REG_CORRECT_RESULT, correct) \
+    mov REG_TEST_NUM, num;    \
+    jne testreg, REG_CORRECT_RESULT, fail \
+    TEST_PSW(num, correct_psw) \
 
 #define LI(reg, val) \
-    mov.u reg, hi:val; \
-    addih reg, reg, lo:val;
+    mov.u reg, lo:val; \
+    movh REG_TEMP_LI, hi:val; \
+    or reg, reg, REG_TEMP_LI; \
 
 #define TEST_PASSFAIL \
         j pass; \
 fail: \
-        movh %d13, 0xf000;  \
-        addi %d13, %d13, 0x1337; \
-        mov.a %a15, %d13;       \
-        st.w [%a15], %d14;      \
-        debug;                   \
-        j fail;                  \
+        LI(REG_TEMP, 0xf0001337)          \
+        mov.a REG_DEV_ADDR, REG_TEMP;     \
+        st.w [REG_DEV_ADDR], REG_TEST_NUM;\
+        debug;                            \
+        j fail;                           \
 pass: \
-        movh %d13, 0xf000;  \
-        addi %d13, %d13, 0x1337; \
-        mov.a %a15, %d13;       \
-        mov %d14, 0;            \
-        st.w [%a15], %d14;      \
-        debug;                  \
-        j pass;
+        LI(REG_TEMP, 0xf0001337)          \
+        mov.a REG_DEV_ADDR, REG_TEMP;     \
+        mov REG_TEST_NUM, 0;              \
+        st.w [REG_DEV_ADDR], REG_TEST_NUM;\
+        debug;                            \
+        j pass;                           \
 
 
 #define TEST_R(insn, num, result, rs1) \
-    TEST_CASE(num, %d1, result,        \
-    LI(%d0, rs1); \
-    insn %d1, %d0; \
+    TEST_CASE(num, REG_CALC_RESULT, result,        \
+    LI(REG_RS1, rs1); \
+    insn REG_CALC_RESULT, REG_RS1; \
     )
 
 #define TEST_RR(insn, num, result, rs1, rs2) \
-    TEST_CASE(num, %d1, result,        \
-    LI(%d0, rs1); \
-    LI(%d1, rs2); \
-    insn %d1, %d0, %d1; \
+    TEST_CASE(num, REG_CALC_RESULT, result,        \
+    LI(REG_RS1, rs1); \
+    LI(REG_RS2, rs2); \
+    insn REG_CALC_RESULT, REG_RS1, REG_RS2; \
     )
+
+#define TEST_R_PSW(insn, num, result, psw, rs1) \
+    TEST_CASE_PSW(num, REG_CALC_RESULT, result, psw         \
+    LI(REG_RS1, rs1); \
+    rstv;         \
+    insn REG_CORRECT_RESULT, REG_RS1; \
+    )
+
